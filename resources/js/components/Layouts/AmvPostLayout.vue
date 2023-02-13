008914bbce21b6847e34a8a0d598ccf797368276
mohamed ELHAZZAZI <aminemohamedf7@gmail.com>
2023-02-13 20:21:28 +0100

    share posts link

--- resources/js/components/Layouts/AmvPostLayout.vue
+++ resources/js/components/Layouts/AmvPostLayout.vue
@@ -88,7 +88,7 @@
         </div>
         <video :src="'../storage/' + post.video" controls></video>
         <div
-            class="w-full h-12 flex justify-between border-t border-gray-300 mt-3"
+            class="w-full h-12 flex justify-between border-t border-gray-300 mt-3 relative"
         >
             <button
                 class="flex items-center gap-1 hover:bg-lightGray mt-2 px-2 hover:fill-red-600 rounded"
@@ -130,6 +130,7 @@
                 {{ post.dislikes }}
             </button>
             <button
+                @click="showShareBox = !showShareBox"
                 class="flex items-center gap-1 hover:bg-lightGray mt-2 px-2 rounded pr-2"
             >
                 <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24">
@@ -138,6 +139,98 @@
                     />
                 </svg>
             </button>
+            <div
+                v-show="showShareBox"
+                class="z-40 absolute right-0 -top-[155px] w-40 py-1 bg-white rounded border-gray-300 border"
+            >
+                <ul class="w-full flex flex-col gap-[2px]">
+                    <li class="w-full h-12">
+                        <button
+                            class="w-full h-full flex items-center px-3 gap-2 text-prussianBlue hover:bg-lightGray"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 48 48"
+                                width="35px"
+                                height="35px"
+                                fill-rule="evenodd"
+                                clip-rule="evenodd"
+                            >
+                                <path
+                                    fill="#fff"
+                                    d="M4.868,43.303l2.694-9.835C5.9,30.59,5.026,27.324,5.027,23.979C5.032,13.514,13.548,5,24.014,5c5.079,0.002,9.845,1.979,13.43,5.566c3.584,3.588,5.558,8.356,5.556,13.428c-0.004,10.465-8.522,18.98-18.986,18.98c-0.001,0,0,0,0,0h-0.008c-3.177-0.001-6.3-0.798-9.073-2.311L4.868,43.303z"
+                                />
+                                <path
+                                    fill="#fff"
+                                    d="M4.868,43.803c-0.132,0-0.26-0.052-0.355-0.148c-0.125-0.127-0.174-0.312-0.127-0.483l2.639-9.636c-1.636-2.906-2.499-6.206-2.497-9.556C4.532,13.238,13.273,4.5,24.014,4.5c5.21,0.002,10.105,2.031,13.784,5.713c3.679,3.683,5.704,8.577,5.702,13.781c-0.004,10.741-8.746,19.48-19.486,19.48c-3.189-0.001-6.344-0.788-9.144-2.277l-9.875,2.589C4.953,43.798,4.911,43.803,4.868,43.803z"
+                                />
+                                <path
+                                    fill="#cfd8dc"
+                                    d="M24.014,5c5.079,0.002,9.845,1.979,13.43,5.566c3.584,3.588,5.558,8.356,5.556,13.428c-0.004,10.465-8.522,18.98-18.986,18.98h-0.008c-3.177-0.001-6.3-0.798-9.073-2.311L4.868,43.303l2.694-9.835C5.9,30.59,5.026,27.324,5.027,23.979C5.032,13.514,13.548,5,24.014,5 M24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974 M24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974 M24.014,4C24.014,4,24.014,4,24.014,4C12.998,4,4.032,12.962,4.027,23.979c-0.001,3.367,0.849,6.685,2.461,9.622l-2.585,9.439c-0.094,0.345,0.002,0.713,0.254,0.967c0.19,0.192,0.447,0.297,0.711,0.297c0.085,0,0.17-0.011,0.254-0.033l9.687-2.54c2.828,1.468,5.998,2.243,9.197,2.244c11.024,0,19.99-8.963,19.995-19.98c0.002-5.339-2.075-10.359-5.848-14.135C34.378,6.083,29.357,4.002,24.014,4L24.014,4z"
+                                />
+                                <path
+                                    fill="#40c351"
+                                    d="M35.176,12.832c-2.98-2.982-6.941-4.625-11.157-4.626c-8.704,0-15.783,7.076-15.787,15.774c-0.001,2.981,0.833,5.883,2.413,8.396l0.376,0.597l-1.595,5.821l5.973-1.566l0.577,0.342c2.422,1.438,5.2,2.198,8.032,2.199h0.006c8.698,0,15.777-7.077,15.78-15.776C39.795,19.778,38.156,15.814,35.176,12.832z"
+                                />
+                                <path
+                                    fill="#fff"
+                                    fill-rule="evenodd"
+                                    d="M19.268,16.045c-0.355-0.79-0.729-0.806-1.068-0.82c-0.277-0.012-0.593-0.011-0.909-0.011c-0.316,0-0.83,0.119-1.265,0.594c-0.435,0.475-1.661,1.622-1.661,3.956c0,2.334,1.7,4.59,1.937,4.906c0.237,0.316,3.282,5.259,8.104,7.161c4.007,1.58,4.823,1.266,5.693,1.187c0.87-0.079,2.807-1.147,3.202-2.255c0.395-1.108,0.395-2.057,0.277-2.255c-0.119-0.198-0.435-0.316-0.909-0.554s-2.807-1.385-3.242-1.543c-0.435-0.158-0.751-0.237-1.068,0.238c-0.316,0.474-1.225,1.543-1.502,1.859c-0.277,0.317-0.554,0.357-1.028,0.119c-0.474-0.238-2.002-0.738-3.815-2.354c-1.41-1.257-2.362-2.81-2.639-3.285c-0.277-0.474-0.03-0.731,0.208-0.968c0.213-0.213,0.474-0.554,0.712-0.831c0.237-0.277,0.316-0.475,0.474-0.791c0.158-0.317,0.079-0.594-0.04-0.831C20.612,19.329,19.69,16.983,19.268,16.045z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+
+                            Whatssapp
+                        </button>
+                    </li>
+                    <li class="w-full h-12">
+                        <button
+                            class="w-full h-full flex items-center px-3 gap-2 text-prussianBlue hover:bg-lightGray"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 48 48"
+                                width="30px"
+                                height="30px"
+                            >
+                                <path
+                                    fill="#039be5"
+                                    d="M24 5A19 19 0 1 0 24 43A19 19 0 1 0 24 5Z"
+                                />
+                                <path
+                                    fill="#fff"
+                                    d="M26.572,29.036h4.917l0.772-4.995h-5.69v-2.73c0-2.075,0.678-3.915,2.619-3.915h3.119v-4.359c-0.548-0.074-1.707-0.236-3.897-0.236c-4.573,0-7.254,2.415-7.254,7.917v3.323h-4.701v4.995h4.701v13.729C22.089,42.905,23.032,43,24,43c0.875,0,1.729-0.08,2.572-0.194V29.036z"
+                                />
+                            </svg>
+                            Facebook
+                        </button>
+                    </li>
+                    <li class="w-full h-12">
+                        <button
+                            @click="copyShareLink"
+                            class="w-full h-full flex items-center px-3 gap-2 text-prussianBlue hover:bg-lightGray"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="w-5 h-5"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+                                />
+                            </svg>
+
+                            <span v-if="!showCopied">Copy link</span>
+                            <span v-else>Copied!</span>
+                        </button>
+                    </li>
+                </ul>
+            </div>
         </div>
         <ConfirmationModalLayout
             ref="deleteModel"
@@ -149,13 +242,18 @@
 
 <script setup>
 import { computed, onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
 import store from "../../store";
 import ConfirmationModalLayout from "./ConfirmationModalLayout.vue";
 const props = defineProps(["post"]);
 const showPostBox = ref(false);
 const text = ref(null);
+const showShareBox = ref(false);
 const deleteModel = ref();
 const emit = defineEmits(["deletePost", "modifyPost"]);
+const showCopied = ref(false);
+const shareLink = ref(window.location.href + "/p/" + props.post.id);
+console.log(shareLink.value);
 const user = computed(() => {
     return store.state.user.info;
 });
@@ -180,6 +278,15 @@ function deletePost() {
 function showModifyModel() {
     showPostBox.value = false;
     emit("modifyPost", props.post);
+}
+
+function copyShareLink() {
+    navigator.clipboard.writeText(shareLink.value);
+    showCopied.value = true;
+    showShareBox.value = false;
+    setInterval(() => {
+        showCopied.value = false;
+    }, 2000);
 }
 </script>
 
