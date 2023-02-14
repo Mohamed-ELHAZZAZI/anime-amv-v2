8c8df23458196671dc03efed92e53049472e77a2
mohamed ELHAZZAZI <aminemohamedf7@gmail.com>
2023-02-14 22:58:15 +0100

    display comments info

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Comment extends Model
 {
@@ -18,6 +19,10 @@ class Comment extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->diffForHumans();
+    }
 
     public function replies()
     {
--- resources/js/components/Layouts/CommentBoxLayout.vue
+++ resources/js/components/Layouts/CommentBoxLayout.vue
@@ -31,10 +31,14 @@
                     <span class="sr-only">Close modal</span>
                 </button>
             </div>
-            <div class="w-full h-[450px] overflow-auto flex py-2">
-                <div class="antialiased mx-auto max-w-screen-sm">
+            <div class="w-full h-[450px] overflow-auto px-12 flex py-2">
+                <div class="antialiased mx-auto w-full">
                     <div class="space-y-4">
-                        <div class="flex">
+                        <div
+                            class="flex"
+                            v-for="comment in comments"
+                            :key="comment.id"
+                        >
                             <div class="flex-shrink-0 mr-3">
                                 <img
                                     class="mt-2 rounded-full w-8 h-8 sm:w-10 sm:h-10"
@@ -45,15 +49,16 @@
                             <div
                                 class="flex-1 border rounded-lg px-4 py-2 sm:px-6 sm:py-4 leading-relaxed"
                             >
-                                <strong>Sarah</strong>
-                                <span class="text-xs text-gray-400"
-                                    >3:34 PM</span
-                                >
+                                <span class="text-base font-bold">{{
+                                    comment.user.firstName +
+                                    " " +
+                                    comment.user.lastName
+                                }}</span>
+                                <span class="ml-2 text-xs text-gray-400">{{
+                                    comment.created_at
+                                }}</span>
                                 <p class="text-sm">
-                                    Lorem ipsum dolor sit amet, consetetur
-                                    sadipscing elitr, sed diam nonumy eirmod
-                                    tempor invidunt ut labore et dolore magna
-                                    aliquyam erat, sed diam voluptua.
+                                    {{ comment.body }}
                                 </p>
 
                                 <h4
@@ -63,7 +68,11 @@
                                 </h4>
 
                                 <div class="space-y-4">
-                                    <div class="flex">
+                                    <div
+                                        class="flex"
+                                        v-for="reply in comment.replies"
+                                        :key="reply.id"
+                                    >
                                         <div class="flex-shrink-0 mr-3">
                                             <img
                                                 class="mt-3 rounded-full w-6 h-6 sm:w-8 sm:h-8"
@@ -74,42 +83,17 @@
                                         <div
                                             class="flex-1 bg-gray-100 rounded-lg px-4 py-2 sm:px-6 sm:py-4 leading-relaxed"
                                         >
-                                            <strong>Sarah</strong>
-                                            <span class="text-xs text-gray-400"
-                                                >3:34 PM</span
+                                            <span class="text-base font-bold">{{
+                                                reply.user.firstName +
+                                                " " +
+                                                reply.user.lastName
+                                            }}</span>
+                                            <span
+                                                class="ml-2 text-xs text-gray-400"
+                                                >{{ reply.created_at }}</span
                                             >
                                             <p class="text-xs sm:text-sm">
-                                                Lorem ipsum dolor sit amet,
-                                                consetetur sadipscing elitr, sed
-                                                diam nonumy eirmod tempor
-                                                invidunt ut labore et dolore
-                                                magna aliquyam erat, sed diam
-                                                voluptua.
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div class="flex">
-                                        <div class="flex-shrink-0 mr-3">
-                                            <img
-                                                class="mt-3 rounded-full w-6 h-6 sm:w-8 sm:h-8"
-                                                src="https://images.unsplash.com/photo-1604426633861-11b2faead63c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&h=200&q=80"
-                                                alt=""
-                                            />
-                                        </div>
-                                        <div
-                                            class="flex-1 bg-gray-100 rounded-lg px-4 py-2 sm:px-6 sm:py-4 leading-relaxed"
-                                        >
-                                            <strong>Sarah</strong>
-                                            <span class="text-xs text-gray-400"
-                                                >3:34 PM</span
-                                            >
-                                            <p class="text-xs sm:text-sm">
-                                                Lorem ipsum dolor sit amet,
-                                                consetetur sadipscing elitr, sed
-                                                diam nonumy eirmod tempor
-                                                invidunt ut labore et dolore
-                                                magna aliquyam erat, sed diam
-                                                voluptua.
+                                                {{ reply.body }}
                                             </p>
                                         </div>
                                     </div>
@@ -132,10 +116,10 @@ import CommentSkeleton from "../skeletons/CommentSkeleton.vue";
 const emit = defineEmits(["hideComment"]);
 const props = defineProps(["post_id"]);
 const showCommentSkeleton = ref(false);
-const comments = ref(null);
+const comments = ref([]);
 onMounted(() => {
     store.dispatch("getComments", props.post_id).then((res) => {
-        comments.value = res.data;
+        comments.value = res.data.comments;
         console.log(comments.value);
     });
 });
